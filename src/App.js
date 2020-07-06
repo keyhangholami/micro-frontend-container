327165781ed0f36903967262e7980398f6d4b1db
Keyhan Gholami <keyhangholami@gmail.com>
2020-07-06 14:00:59 +0200

    MF setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, BrowserRouter, Route, Switch } from 'react-router-dom';
+import MicroFrontend from './MicroFrontend';
 
-function App() {
+const {
+  REACT_APP_CREATEREACTAPP_HOST: createReactAppHost,
+  REACT_APP_CREATEVUEAPP_HOST: createVueAppHost } = process.env;
+
+const CreateReactApp = ({ history }) => (
+  <MicroFrontend
+    history={history}
+    host={createReactAppHost}
+    name="createReactApp"
+  />
+);
+const CreateVueApp = ({ history }) => (
+  <MicroFrontend
+    history={history}
+    host={createVueAppHost}
+    name="createVueApp"
+  />
+);
+
+const Home = () => (
+  <>
+    <p>
+      What is a micro front-ends approach? The term micro front-ends first came
+      up in the ThoughtWorks Technology Radar in November 2016. It extends the
+      concepts of microservices to front-end development.
+    </p>
+    <p>
+      The approach is to split the browser-based code into micro front-ends by
+      breaking down application features. By making smaller and feature-centered
+      codebases, we achieve the software development goal of decoupling.
+    </p>
+    <p>
+      Although the codebases are decoupled, the user experiences are coherent.
+      In addition, each codebase can be implemented, upgraded, updated, and
+      deployed independently.
+    </p>
+    <p>
+      Here is the paradise of micro front-ends. JavaScript applications,
+      regardless of frameworks and versions, are launched by a container. These
+      applications, legacy and new, work together seamlessly, and act like one
+      application.
+    </p>
+  </>
+);
+
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>
+        This is an example of micro frontend.
+      </h1>
+      <p>
+        In the links below, Home is a component bundled with the App Container,
+        and Micro Frontend is a micro frontend app loaded from an outside route.
+      </p>
+      <ul>
+        <li>
+          <NavLink to="/home">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/createReactApp">
+            Micro Frontend: Create React App
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/createVueApp">
+            Micro Frontend: Create Vue App
+          </NavLink>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/createReactApp" render={() => <CreateReactApp />} />
+        <Route path="/createVueApp" render={() => <CreateVueApp />} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
